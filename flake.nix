d2b164db17f3053874a1c90a4cf1c739fec3cb25
Louis Thevenet <louis.tvnt@gmail.com>
2025-07-12 19:43:02 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        let
+          rust-toolchain = pkgs.symlinkJoin {
+            name = "rust-toolchain";
+            paths = with pkgs; [
+              rustc
+              rustfmt
+              cargo
+              cargo-watch
+              rust-analyzer
+              rustPlatform.rustcSrc
+              cargo-dist
+              cargo-tarpaulin
+              cargo-insta
+              cargo-machete
+              cargo-edit
+              cargo-flamegraph
+            ];
+          };
+
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            RUST_BACKTRACE = "full";
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+            packages = [
+              rust-toolchain
+              pkgs.clippy
+              pkgs.hyperfine
+              pkgs.flamelens
+            ];
+          };
+        };
+    };
+}
