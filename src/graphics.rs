231a2e1719d05f36bc807526bcf01e804d2d536c
Louis Thevenet <louis.tvnt@gmail.com>
2025-08-04 13:29:00 +0200

    feat: add zoom/unzoom

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,6 +1,7 @@
 use pixels::{Error, Pixels, SurfaceTexture};
 use rayon::iter::{IndexedParallelIterator, ParallelIterator};
 use rayon::slice::ParallelSliceMut;
+use std::time::{Duration, Instant};
 use winit::dpi::PhysicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
@@ -17,6 +18,7 @@ pub struct CityExplorer {
     city: City,
     window_size: (u32, u32),
     pixels: Vec<u8>,
+    zoom_factor: f32,
 }
 
 pub fn start_city_explorer(city: City) -> Result<(), Error> {
@@ -50,61 +52,56 @@ pub fn start_city_explorer(city: City) -> Result<(), Error> {
             }
         }
 
-        // Handle input events
-        let mut delta = (0, 0);
+        let mut delta_x = 0;
+        let mut delta_y = 0;
         if input.update(&event) {
-            // Handle keyboard events
             if input.key_pressed(KeyCode::ArrowLeft) || input.key_held(KeyCode::ArrowLeft) {
-                delta.0 = -10;
+                delta_x += -30;
             }
             if input.key_pressed(KeyCode::ArrowRight) || input.key_held(KeyCode::ArrowRight) {
-                delta.0 = 10;
+                delta_x += 30;
             }
             if input.key_pressed(KeyCode::ArrowUp) || input.key_held(KeyCode::ArrowUp) {
-                delta.1 = -10;
+                delta_y += -30;
             }
             if input.key_pressed(KeyCode::ArrowDown) || input.key_held(KeyCode::ArrowDown) {
-                delta.1 = 10;
+                delta_y += 30;
             }
             if input.key_pressed(KeyCode::Escape) || input.close_requested() {
                 elwt.exit();
                 return;
             }
-            let delta = match delta {
-                (0, 0) => None,
-                (x, y) => Some((
-                    x * (if input.key_held(KeyCode::ShiftLeft) {
-                        2
-                    } else {
-                        1
-                    }),
-                    y * (if input.key_held(KeyCode::ShiftLeft) {
-                        2
-                    } else {
-                        1
-                    }),
-                )),
-            };
-
-            city_explorer.redraw_pixels(delta);
-
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                city_explorer.resize(size);
-                println!("Window resized");
-                if let Err(_err) = pixels.resize_buffer(size.width, size.height) {
-                    elwt.exit();
-                    return;
-                }
-                if let Err(_err) = pixels.resize_surface(size.width, size.height) {
-                    elwt.exit();
-                    return;
-                }
-            }
-            // Update internal state and request a redraw
-            city_explorer.update(delta);
-            window.request_redraw();
         }
+        let zoom_delta = input.scroll_diff().1 * 0.05;
+
+        let speed_multiplier = if input.key_held(KeyCode::ShiftLeft) {
+            2
+        } else {
+            1
+        };
+        let zoom_adjusted_speed = (speed_multiplier as f32 / city_explorer.zoom_factor) as i32;
+        delta_x *= zoom_adjusted_speed;
+        delta_y *= zoom_adjusted_speed;
+
+        city_explorer.update((delta_x, delta_y), zoom_delta);
+        if delta_x != 0 || delta_y != 0 || zoom_delta != 0.0 {
+            city_explorer.redraw_pixels();
+        }
+
+        // Resize the window
+        if let Some(size) = input.window_resized() {
+            city_explorer.resize(size);
+            println!("Window resized");
+            if let Err(_err) = pixels.resize_buffer(size.width, size.height) {
+                elwt.exit();
+                return;
+            }
+            if let Err(_err) = pixels.resize_surface(size.width, size.height) {
+                elwt.exit();
+                return;
+            }
+        }
+        window.request_redraw();
     });
     res.map_err(|e| Error::UserDefined(Box::new(e)))
 }
@@ -118,28 +115,38 @@ impl CityExplorer {
             origin: (0, 0),
             window_size,
             pixels: vec![0; (window_size.0 * window_size.1 * 4) as usize],
+            zoom_factor: 1.0,
         };
-        res.redraw_pixels(None);
+        res.redraw_pixels();
         res
     }
 
-    fn update(&mut self, delta: Option<(i8, i8)>) {
-        self.origin.0 += delta.unwrap_or((0, 0)).0 as i32;
-        self.origin.1 += delta.unwrap_or((0, 0)).1 as i32;
+    fn update(&mut self, delta: (i32, i32), zoom_delta: f32) {
+        self.origin.0 += delta.0 as i32;
+        self.origin.1 += delta.1 as i32;
+        self.zoom_factor += zoom_delta;
+        self.zoom_factor = self.zoom_factor.clamp(0.1, 30.0);
     }
 
     /// There is potential to clerverly use the `delta` parameter to only redraw the pixels that have changed. But for now, it's okay as it is.
-    fn redraw_pixels(&mut self, _delta: Option<(i8, i8)>) {
+    fn redraw_pixels(&mut self) {
         self.pixels
             .par_chunks_mut(4)
             .enumerate()
             .for_each(|(i, pixel)| {
                 let x_frame = (i % self.window_size.0 as usize) as i32;
                 let y_frame = (i / self.window_size.0 as usize) as i32;
-                let x1 = self.origin.0 - (self.window_size.0 as i32 / 2);
-                let y1 = self.origin.1 - (self.window_size.1 as i32 / 2);
-
-                let rgba = match self.city.is_something.get(&(x1 + x_frame, y1 + y_frame)) {
+                let x1 = {
+                    let half_width = self.window_size.0 as f32 / 2.0;
+                    let world_offset = (x_frame as f32 - half_width) / self.zoom_factor;
+                    self.origin.0 + world_offset as i32
+                };
+                let y1 = {
+                    let half_height = self.window_size.1 as f32 / 2.0;
+                    let world_offset = (y_frame as f32 - half_height) / self.zoom_factor;
+                    self.origin.1 + world_offset as i32
+                };
+                let rgba = match self.city.is_something.get(&(x1, y1)) {
                     Some(CellType::Building) => [255, 0, 0, 255],
                     Some(CellType::Road) => [0, 255, 0, 255],
                     None => [0, 0, 0, 0],
@@ -148,7 +155,6 @@ impl CityExplorer {
                 pixel.copy_from_slice(&rgba);
             });
     }
-
     /// Draw the `World` state to the frame buffer.
     ///
     /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
@@ -160,6 +166,6 @@ impl CityExplorer {
         self.window_size = (new_size.width, new_size.height);
         self.pixels
             .resize((self.window_size.0 * self.window_size.1 * 4) as usize, 0);
-        self.redraw_pixels(None);
+        self.redraw_pixels();
     }
 }
